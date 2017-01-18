cb39f22aa97805df766a20a035a3e1e9700bf424
Adam Brin <abrin@digitalantiquity.org>
2017-01-18 08:17:17 -0700

    Getting to green

--- src/test/java/org/dataarc/web/controller/ControllerTest.java
+++ src/test/java/org/dataarc/web/controller/ControllerTest.java
@@ -1,7 +1,6 @@
 package org.dataarc.web.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,8 +17,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebAppConfiguration
 @ContextConfiguration(classes = { DataArcWebConfig.class })
@@ -49,12 +52,22 @@ public class ControllerTest extends AbstractServiceTest {
         query.getConditions().add(new QueryPart("sites.SiteCode", "SITE000572", MatchType.CONTAINS));
         query.setSchema(schema);
 
-        mockMvc.perform(post(UrlConstants.QUERY_DATASTORE, query))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+        mockMvc.perform(MockMvcRequestBuilders.post(UrlConstants.QUERY_DATASTORE).content(asJsonString(query)).contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8));
 
 //        mockMvc.perform(get("/index").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith("application/json"))
 //        .andExpect(jsonPath("$.greeting").value("Hello World"));
 
+    }
+    
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
