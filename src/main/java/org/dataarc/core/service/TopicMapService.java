af23d48312c6440d64ee5efc2f0fdbb7831a1b47
Adam Brin <adam@brin.org>
2017-01-17 18:38:54 -0700

    Topic map setup

---
+++ src/main/java/org/dataarc/core/service/TopicMapService.java
@@ -0,0 +1,12 @@
+package org.dataarc.core.service;
+
+import org.springframework.stereotype.Service;
+import org.tmapiutils.impexp.xtm.XTMSerializer;
+
+@Service
+public class TopicMapService {
+
+    public void load(String file) {
+//        XTM
+    }
+}
