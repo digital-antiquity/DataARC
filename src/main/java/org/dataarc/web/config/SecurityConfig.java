2a2cd856e53ab8c8af3519edf394082c0b965159
Adam Brin <adam@brin.org>
2017-01-17 20:33:17 -0700

    Fixing mvc; disabling csrf

--- src/main/java/org/dataarc/web/config/SecurityConfig.java
+++ src/main/java/org/dataarc/web/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +21,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
         web.ignoring().antMatchers("/js/**","/css/**","/components/**","/data/**","/","/json");
     }
+
+    @Override
+    public void configure(HttpSecurity web) throws Exception {
+        super.configure(web);
+        web.csrf().disable();
+    }
+
+    
 }
