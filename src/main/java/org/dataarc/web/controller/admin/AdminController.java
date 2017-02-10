86da685846a7582993644b9d0a3ee7a7b4e3343b
Adam Brin <abrin@digitalantiquity.org>
2017-02-10 11:01:22 -0700

    adding admin controller with reindex

--- src/main/java/org/dataarc/web/controller/admin/AdminController.java
+++ src/main/java/org/dataarc/web/controller/admin/AdminController.java
@@ -1,15 +1,16 @@
 package org.dataarc.web.controller.admin;
 
+import org.dataarc.web.AbstractController;
 import org.dataarc.web.api.schema.UrlConstants;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AdminController {
+public class AdminController extends AbstractController {
 
     @RequestMapping(UrlConstants.ADMIN)
     public String index() {
-        return "index";
+        return "admin/index";
     }
 
 }
---
+++ src/main/webapp/WEB-INF/content/admin/index.ftl
@@ -0,0 +1,6 @@
+<h3> Admin functions</h3>
+
+<h5>Reindex</h5>
+<form action="${contextPath}/api/admin/reindex" method="POST">
+<input type="submit" name="reindex" value="reindex" />
+</form>
