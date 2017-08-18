aa28be5792a3306dbdc6a3efb476d9b6f8f1aef9
Adam Brin <abrin@digitalantiquity.org>
2017-08-18 12:07:02 -0700

    fixing login redirect

--- src/main/java/org/dataarc/web/security/SecurityConfig.java
+++ src/main/java/org/dataarc/web/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.atlassian.crowd.integration.http.CrowdHttpAuthenticator;
 import com.atlassian.crowd.integration.http.CrowdHttpAuthenticatorImpl;
@@ -57,7 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             and().formLogin().loginPage("/login")
             .permitAll().defaultSuccessUrl("/mapping").and().logout().permitAll();
             web.exceptionHandling()
-            .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+            .defaultAuthenticationEntryPointFor(new RestAuthenticationEntryPoint(), new AntPathRequestMatcher("/api/**"))
             .accessDeniedHandler(new RestAccessDeniedHandler());
         }
     }
