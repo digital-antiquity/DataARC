c9c7fc46e300f15c43be86909d2c1c4c4f929a1f
Adam Brin <abrin@digitalantiquity.org>
2017-10-19 08:04:48 -0700

    better docker setup

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -7,6 +7,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     ALTER USER dataarc WITH PASSWORD 'dataarc';
     GRANT ALL PRIVILEGES ON DATABASE dataarc TO dataarc;
 EOSQL
-mkdir /var/lib/postgresql/backups/
-echo  "0 1 * * * pg_dump -U dataarc dataarc > /var/lib/postgresql/backups/\`date  +%Y-%m-%d\`.sql" > .crontab 
+mkdir -pv /var/lib/postgresql/backups/data/
+echo  "0 1 * * * pg_dump -U dataarc dataarc > /var/lib/postgresql/data/backups/\`date  +%Y-%m-%d\`.sql" > .crontab 
+/etc/init.d/cron start
 crontab .crontab
