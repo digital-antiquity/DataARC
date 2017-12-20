4d884330a8540be44a220b5d324f97596f583491
Adam Brin <abrin@digitalantiquity.org>
2017-12-20 08:32:05 -0700

    tuning debug

--- src/main/java/org/dataarc/core/search/SolrIndexingService.java
+++ src/main/java/org/dataarc/core/search/SolrIndexingService.java
@@ -292,16 +292,16 @@ public class SolrIndexingService {
             }
             String val = template.apply(properties);
             doc.setTitle(val);
-            if (StringUtils.containsIgnoreCase(source.getName(), "bone")) {
-                logger.debug(source.getTitleTemplate());
-                logger.debug("{}", properties.keySet());
-            }
-            logger.debug("{}  ---- {}", val, source.getName());
+//            if (StringUtils.containsIgnoreCase(source.getName(), "bone")) {
+//                logger.debug(source.getTitleTemplate());
+//                logger.debug("{}", properties.keySet());
+//            }
+            logger.trace("{}  ---- {}", val, source.getName());
         } catch (IOException e) {
             logger.debug("{}", properties);
             logger.debug(source.getTitleTemplate());
             // TODO Auto-generated catch block
-            e.printStackTrace();
+            logger.error("{}", e,e);
         }
 
     }
