b8435d6863a5989cc4abc4f74e3ad3757f451982
Adam Brin <abrin@digitalantiquity.org>
2018-01-19 10:11:24 -0700

    fixing resetStyle on geogrpahy

--- src/main/webapp/js/geography.js
+++ src/main/webapp/js/geography.js
@@ -137,7 +137,9 @@ pLayer.prototype = {
   },
   resetStyles: function() {
     this.selected = false;
-    this.layer.setStyle(this.normalStyle);
+    if (this.layer != undefined) {
+        this.layer.setStyle(this.normalStyle);
+    }
   },
 	handlePolyClick(e){
 		var popup = e.layer.getPopup();
