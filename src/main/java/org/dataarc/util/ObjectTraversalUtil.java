a41d14b9c573f18ea35f65774d42083df7b31d1a
Adam Brin <adam@brin.org>
2018-03-09 08:06:08 -0700

    cleanup of NPE

--- src/main/java/org/dataarc/util/ObjectTraversalUtil.java
+++ src/main/java/org/dataarc/util/ObjectTraversalUtil.java
@@ -11,12 +11,13 @@ import org.apache.commons.lang3.StringUtils;
 
 public class ObjectTraversalUtil {
 
-    public static void traverse(Map<String, Object> properties, Map<String,Object> newMap, FieldDataCollector collector) {
+    public static void traverse(Map<String, Object> properties, Map<String, Object> newMap, FieldDataCollector collector) {
         traverseMap(properties, newMap, collector, null, null);
     }
 
     @SuppressWarnings("unchecked")
-    public static void traverseMap(Map<String, Object> properties,Map<String,Object> newMap, final FieldDataCollector collect, final String _parent, String parentKey) {
+    public static void traverseMap(Map<String, Object> properties, Map<String, Object> newMap, final FieldDataCollector collect, final String _parent,
+            String parentKey) {
         String __parent = _parent;
         if (__parent == null) {
             __parent = "";
@@ -28,34 +29,45 @@ public class ObjectTraversalUtil {
         final String parent = __parent;
         properties.keySet().forEach(key -> {
             Object arg2 = properties.get(key);
-            String normapName = collect.add(parent, key, arg2);
-            if (StringUtils.contains(normapName, ".")) {
-                normapName = StringUtils.substringAfterLast(normapName, ".");
-            }
-            if (arg2 instanceof Collection) {
-                List<Object> childList = new ArrayList<>();
-                newMap.put(normapName, childList);
-
-                ((Collection<Object>) arg2).forEach(new Consumer<Object>() {
-
-                    @Override
-                    public void accept(Object t) {
-                        if (t instanceof Map<?, ?>) {
-                            Map<String,Object> childMap = new HashMap<>();
-                            childList.add(childMap);
-                            Map<String, Object> map = (Map<String, Object>) t;
-                            traverseMap(map, childMap, collect, parent, key);
-                        }
+            String normapName_ = collect.add(parent, key, arg2);
+            if (normapName_ != null) {
+                String normapName = new String(normapName_);
+                if (StringUtils.contains(normapName, ".")) {
+                    normapName = StringUtils.substringAfterLast(normapName, ".");
+                }
+                if (arg2 instanceof Collection) {
+                    List<Object> childList = new ArrayList<>();
+                    String term = normapName;
+                    if (StringUtils.isBlank(term)) {
+                        term = StringUtils.substringBeforeLast(normapName_, ".");
                     }
+                    if (StringUtils.isBlank(term)) {
+                        term = normapName_;
+                    }
+                    System.out.println(term + "  " + normapName_ + " " + normapName);
+                    newMap.put(term, childList);
 
-                });
-            } else if (arg2 instanceof Map) {
-                Map<String,Object> childMap = new HashMap<>();
-                newMap.put(normapName, childMap);
-                traverseMap((Map<String, Object>) arg2, childMap, collect, parent, key);
-            } else {
-                if (normapName != null) {
-                    newMap.put(normapName, properties.get(key));
+                    ((Collection<Object>) arg2).forEach(new Consumer<Object>() {
+
+                        @Override
+                        public void accept(Object t) {
+                            if (t instanceof Map<?, ?>) {
+                                Map<String, Object> childMap = new HashMap<>();
+                                childList.add(childMap);
+                                Map<String, Object> map = (Map<String, Object>) t;
+                                traverseMap(map, childMap, collect, parent, key);
+                            }
+                        }
+
+                    });
+                } else if (arg2 instanceof Map) {
+                    Map<String, Object> childMap = new HashMap<>();
+                    newMap.put(normapName, childMap);
+                    traverseMap((Map<String, Object>) arg2, childMap, collect, parent, key);
+                } else {
+                    if (normapName != null) {
+                        newMap.put(normapName, properties.get(key));
+                    }
                 }
             }
         });
