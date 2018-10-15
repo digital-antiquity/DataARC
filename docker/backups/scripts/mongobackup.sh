cb2f1bf427dfceea2a47ce915cc2cc51ecc74f25
Adam Brin <abrin@digitalantiquity.org>
2018-10-15 12:48:00 -0700

    adding gzip and tar to cron docker

--- docker/backups/scripts/mongobackup.sh
+++ docker/backups/scripts/mongobackup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 DATE=`date +%Y-%m-%d`
 mongodump -h mongo -o /backups/mongo-${DATE}
+tar -cvzf /backups/mongo-{$DATE}.tgz
+rm -Rrf /backups/mongo-${DATE}
--- docker/backups/scripts/pgbackup.sh
+++ docker/backups/scripts/pgbackup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 DATE=`date +%Y-%m-%d`
 pg_dump -h db -U dataarc -f /backups/dataarc-${DATE}.sql dataarc
+gzip /backups/dataarc-${DATE}.sql
