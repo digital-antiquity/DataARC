6c3d66fb6d73832e8b7497b2f4483df9c7ed6808
Adam Brin <abrin@digitalantiquity.org>
2018-10-16 08:32:13 -0700

    fixing backup script

--- docker/backups/scripts/mongobackup.sh
+++ docker/backups/scripts/mongobackup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DATE=`date +%Y-%m-%d`
 mongodump -h mongo -o /backups/mongo-${DATE}
-tar -cvzf /backups/mongo-{$DATE}.tgz
+tar -cvzf /backups/mongo-${DATE}.tgz /backups/mongo-${DATE}/
 rm -Rrf /backups/mongo-${DATE}
