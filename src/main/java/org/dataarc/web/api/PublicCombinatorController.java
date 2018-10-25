df23a6d7b43490b2a6f885c3392530c9098a31de
Adam Brin <adam@brin.org>
2018-10-25 07:00:59 -0700

    renaming controller

--- src/main/java/org/dataarc/web/api/PublicCombinatorController.java
+++ src/main/java/org/dataarc/web/api/PublicCombinatorController.java
@@ -1,127 +1,39 @@
 package org.dataarc.web.api;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
-import org.dataarc.core.search.SolrService;
-import org.dataarc.core.search.query.SearchQueryObject;
-import org.dataarc.util.PersistableUtils;
+import org.apache.commons.collections.CollectionUtils;
+import org.dataarc.bean.Combinator;
+import org.dataarc.core.service.CombinatorService;
+import org.dataarc.util.View;
 import org.dataarc.web.UrlConstants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 @RestController
 public class PublicCombinatorController extends AbstractRestController {
 
     @Autowired
-    private SolrService luceneService;
+    private CombinatorService indicatorService;
 
-    /**
-     * Gets the full data for an ID of a record
-     * 
-     * @param id
-     * @param schemaId
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(path = UrlConstants.GET_ID, method = { RequestMethod.GET, RequestMethod.POST }, produces = { UrlConstants.JSON_UTF8 })
-    public SearchResultObject getId(@RequestParam(value = "id", required = false) String id,
-            @RequestParam(value = "schemaId", required = false) Long schemaId) throws Exception {
-        SearchQueryObject query_ = new SearchQueryObject();
-        query_.setIdAndMap(false);
-        if (StringUtils.isNotBlank(id)) {
-            query_.setIds(Arrays.asList(id));
+    @RequestMapping(path = UrlConstants.LIST_COMBINATORS_PUBLIC, method = RequestMethod.GET)
+    @JsonView(View.Combinator.class)
+    public List<Combinator> getIndicatorById(@RequestParam(value = "id", required = false) List<Long> ids) {
+
+        if (CollectionUtils.isEmpty(ids)) {
+            return indicatorService.findAll();
         }
-        if (schemaId != null) {
-            query_.setSchemaId(schemaId);
-        }
-        return performSearch(query_, null, 1);
-    }
-
-    private SearchResultObject performSearch(SearchQueryObject query_, Integer page, Integer size) {
-        try {
-            SearchQueryObject query = query_;
-            if (query == null) {
-                query = new SearchQueryObject();
-            }
-            if (page != null) {
-                query.setPage(page);
-            }
-            if (size != null) {
-                query.setSize(size);
-            }
-            return luceneService.search(query);
-        } catch (Throwable t) {
-            logger.error("error searching", t);
-        }
-        return null;
-    }
-
-    /**
-     * Performs a normal search for a given request
-     * 
-     * @param query_
-     * @param page
-     * @param size
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(path = UrlConstants.SEARCH, method = RequestMethod.POST, produces = { UrlConstants.JSON_UTF8 },
-            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE })
-    public SearchResultObject searchPost(@RequestBody(required = true) SearchQueryObject query_,
-            @RequestParam(value = "page", required = false) Integer page,
-            @RequestParam(value = "size", required = false) Integer size
-
-    ) throws Exception {
-        try {
-            if (query_ == null || query_.isFindAll()) {
-                File file = new File(System.getProperty("java.io.tmpdir"), "temp-findall.bin");
-                logger.debug("using cache file: {}", file);
-                ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file));
-                SearchResultObject sro = (SearchResultObject) inputStream.readObject();
-                inputStream.close();
-                return sro;
-            }
-        } catch (Exception e) {
-            logger.error("{}", e, e);
-        }
-
-        return performSearch(query_, page, size);
-    }
-
-    /**
-     * Performs a normal search for a given request;
-     * 
-     * assumes that the request has a single source specified, and that it returns some facets plus a page of results
-     * 
-     * @param query_
-     * @param page
-     * @param size
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(path = UrlConstants.SEARCH_RESULTS, method = RequestMethod.POST, produces = { UrlConstants.JSON_UTF8 },
-            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE })
-    public SearchResultObject searchResultsPage(@RequestBody(required = true) SearchQueryObject query_,
-            @RequestParam(value = "page", required = false) Integer page,
-            @RequestParam(value = "size", required = false) Integer size
-
-    ) throws Exception {
-        query_.setResultPage(true);
-        if (PersistableUtils.isNullOrTransient(query_.getSchemaId())) {
-            logger.error("should have a schemaId");
-            return null;
-        }
-        query_.setIdAndMap(false);
-        return performSearch(query_, page, size);
+        List<Combinator> toReturn = new ArrayList<>();
+        ids.forEach(id -> {
+            toReturn.add(indicatorService.view(id));
+        });
+        return toReturn;
     }
 
 }
