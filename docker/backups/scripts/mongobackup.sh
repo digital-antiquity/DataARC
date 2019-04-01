999c39bb18bf28d53c4d53bef62a4089a3fd19c5
Tim Sexton <tim2342@gmail.com>
2019-04-01 15:54:36 -0500

    updated backup scripts

--- docker/backups/scripts/mongobackup.sh
+++ docker/backups/scripts/mongobackup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DATE=`date +%Y-%m-%d`
-mongodump -h mongo -o /backups/mongo-${DATE}
-tar -cvzf /backups/mongo-${DATE}.tgz /backups/mongo-${DATE}/
-rm -Rrf /backups/mongo-${DATE}
+mongodump -h mongo -o /backups/mongo/mongo-${DATE}
+tar -cvzf /backups/mongo/mongo-${DATE}.tgz /backups/mongo/mongo-${DATE}/
+rm -Rrf /backups/mongo/mongo-${DATE}
--- docker/backups/scripts/pgbackup.sh
+++ docker/backups/scripts/pgbackup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DATE=`date +%Y-%m-%d`
-pg_dump -h db -U dataarc -f /backups/dataarc-${DATE}.sql dataarc
-gzip /backups/dataarc-${DATE}.sql
+pg_dump -h db -U dataarc -f /backups/pg/dataarc-${DATE}.sql dataarc
+gzip /backups/pg/dataarc-${DATE}.sql
